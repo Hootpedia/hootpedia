fd26702ed9d5711b48614df41501576ba6dff158
kjlmason <48289560+kjlmason@users.noreply.github.com>
2020-09-26 17:49:50 -0400

    initial commit
    Installed bootstrap, added default index and header file

---
+++ header.php
@@ -0,0 +1,8 @@
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="" type="image/gif" sizes="16x16">
+		
+	
+  		<!-- Bootstrap & Core CSS -->
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
