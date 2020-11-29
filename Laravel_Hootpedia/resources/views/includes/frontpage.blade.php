00dfdae21657c23382e7b22fbde97f25de18c1bd
kjlmason <48289560+kjlmason@users.noreply.github.com>
2020-11-28 22:33:01 -0500

    Front Page + Modal Update
    Added the updated front page with the modals for new posts, and the test loop for grabbing articles. Added Bootstrap javascript to the heading

--- Laravel_Hootpedia/resources/views/includes/frontpage.blade.php
+++ Laravel_Hootpedia/resources/views/includes/frontpage.blade.php
@@ -3,9 +3,11 @@
 	<h1 class="pt-5">Articles/Discussions</h1>
 	<div class="d-inline">
 		<p>Article blocks go here.</p>
-
+		
+		<?php $count = DB::table('articles')->count(); ?>
+		@for ($i = 0; $i < 5; $i++)
 		<!-- Cards for user Discussions -->
-		<div class="card mx-auto d-flex justify-content-center shadow" style="width: 60%; min-width:25rem;">
+		<div class="card mx-auto my-4 d-flex justify-content-center shadow" style="width: 60%; min-width:25rem;">
 			<h2 class="border border-top-0 border-left-0 border-right-0 py-2">Topic</h2>
 			<div class="card-body row text-left">
 				<div class="col-3 text-center p-1">
@@ -18,9 +20,31 @@
 					<p class="small text-secondary">Author Name</p>
 					<p class="card-text">Some quick example text to build on the card title and make up the bulk
 						of the card's content.</p>
-					<a href="#" class="btn btn-sm btn-outline-primary">Read More</a>
+					<button type="button" class="btn btn-sm btn-outline-primary" data-toggle="modal" data-target="#exampleModalCenter">
+					Read More
+					</button>
 				</div>
 			</div>
 		</div>
+		@endfor
 	</div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered w-75" role="document">
+    <div class="modal-content  w-100">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        @include('includes.post')
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
 </div>
