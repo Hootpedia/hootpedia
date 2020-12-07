61e8f3ce14a9f0f6ac1858564f00b921bcc456ac
kjlmason <48289560+kjlmason@users.noreply.github.com>
2020-12-06 22:24:30 -0500

    profile page styling

--- resources/views/includes/profile.blade.php
+++ resources/views/includes/profile.blade.php
@@ -1,26 +1,31 @@
-<form>
-    <br>
-    <p>Profile picture Preview</p>
-    <input type="image" src="https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png" alt="Submit" style="float:inherit" width="250" height="250">
-    <br><br>
-    <label for="fname">First name:</label><br>
-    <input type="text" id="fname" name="fname"><br>
-    <label for="lname">Last name:</label><br>
-    <input type="text" id="lname" name="lname">
-    <br>
-    <form>
-        <br>
-        <input type="radio" id="male" name="gender" value="male">
-        <label for="male">Female</label><br>
-        <input type="radio" id="female" name="gender" value="female">
-        <label for="female">Male</label><br><br>
-        <label for="fname">Age:</label><br>
-        <input type="text" id="fname" name="fname"><br><br>
-        <label for="fname">School:</label><br>
-        <input type="text" id="fname" name="fname"><br>
-        <br><label for="fname">Bio:</label><br>
+<div class="container">
+	<form>
+		<br>
+		<p>Profile picture Preview</p>
+		<input type="image" src="https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png" alt="Submit" style="float:inherit" width="250" height="250">
+		<br><br>
+		<div class="form-group">
+			<label for="fname">First name:</label><br>
+			<input class="form-control" type="text" id="fname" name="fname"><br>
+			<label for="lname">Last name:</label><br>
+			<input class="form-control" type="text" id="lname" name="lname">
+		</div>
+		<br>
+		<form>
+			<div class="form-group">
+				<input class="form-control" type="radio" id="male" name="gender" value="male">
+				<label for="male">Female</label><br>
+				<input class="form-control" type="radio" id="female" name="gender" value="female">
+				<label for="female">Male</label><br><br>
+				<label for="fname">Age:</label><br>
+				<input class="form-control" type="text" id="fname" name="fname"><br><br>
+				<label for="fname">School:</label><br>
+				<input class="form-control" type="text" id="fname" name="fname"><br>
+				<br><label for="fname">Bio:</label><br>
+			</div>
 
-        <input type="text" id="fname" name="fname"><br>
-        <br><input type="submit" value="Submit">
-        <br><br>
-    </form>
+			<input class="form-control" type="text" id="fname" name="fname"><br>
+			<br><input type="submit" value="Submit">
+			<br><br>
+   </form>
+ </div>
