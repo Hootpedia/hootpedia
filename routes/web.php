a2fdbde6d2e54a71408be742b10634052a75915a
Wilmayer Dure <durewill@gmail.com>
2020-12-07 03:39:30 -0500

    edits

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@ use Illuminate\Support\Facades\Route;
 });*/
 
 Route::get('/', function () {
-    return view('home'/*,[
+    return view('home',[
         'articles'=> App\Models\Article::latest()->get()
-    ]*/);
+    ]);
 });
 
 /*Route::get('/newpost', function () {
